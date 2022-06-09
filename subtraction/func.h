9d86fe3903c0e93f9e43953c285430cd2cfe3bd1
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-06-09 23:47:00 +0700

    Commit 2
    Add subtraction in newbranch

---
+++ subtraction/func.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+
+using namespace std;
+
+int subt(int a,int b){
+	int c;
+	c=a-b;
+	return c;
+}
---
+++ subtraction/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "func.h"
+
+using namespace std;
+
+int main(){
+	int a=3;
+	int b=7;
+	cout << "Subt is " <<subt(a,b) << endl;
+	return 0;
+}
